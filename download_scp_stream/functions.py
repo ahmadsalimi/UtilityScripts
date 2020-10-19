c061ded320931a8e5e7320ab792132d0b57e830c
ahmadsalimi <ahsa9978@gmail.com>
2020-10-19 17:53:56 +0330

    update requirments.txt

--- download_scp_stream/functions.py
+++ download_scp_stream/functions.py
@@ -1,14 +1,8 @@
-# from paramiko import SSHClient
 from tqdm import tqdm
 import pysftp
 
 
 def sftp_connect(server, username=None, password=None):
-    # ssh = SSHClient()
-    # ssh.load_system_host_keys()
-    # ssh.connect(server, username=username, password=password)
-    # return ssh.open_sftp()
-
     return pysftp.Connection(server, username=username, password=password)
 
 def send_file(response, sftp, path):
